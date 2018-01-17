bc21c500909ef1c014852e0147033e7ad2a69597
Patcharapol Nirunpornputta <patcharapol.n@ku.th>
2018-01-17 16:54:54 +0700

    Fix some bugs in game solver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -3,19 +3,28 @@ import java.util.Scanner;
 
 /**
  * Automatically find the secret to any NumberGame
+ * 
  * @author Patcharapol Nirunpornputta
  */
 public class GameSolver {
 	/**
 	 * play the game
 	 * 
-	 * @param game a game that users want to play
+	 * @param game
+	 *            a game that users want to play
 	 * @return a secret number guess by users
 	 */
 	public int play(NumberGame game) {
 		Scanner console = new Scanner(System.in);
-		int guess = game.getUpperBound() / 2;
-		int value = guess / 2;
+		int guess = 0, value = 0;
+		if (game.getUpperBound() == 1)
+			guess = game.getUpperBound();
+		else
+			guess = game.getUpperBound() / 2;
+		if (guess / 2 < 1) {
+			value = (int) Math.ceil((double) guess / 2);
+		} else
+			value = guess / 2;
 		System.out.println(game.toString());
 		boolean correct = false;
 		while (correct == false) {
